d838716d5c9c4e65118fc70de04a6af9a68fda6e
Noor amin <noor.amin@markcoders.com>
2024-06-04 20:40:40 +0500

    profile worked done only data

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -12,7 +12,9 @@ const Profile = () => {
     const [yourData, setYourData] = useState([]);
     const user = useSelector((state) => state.user?.user);
     const accessToken = user?.accessToken || "";
-
+    useEffect(()=>{
+        console.log(yourData)
+    },[])
     const dispatch = useDispatch();
     const isSidebarOpen = useSelector((state) => state.sidebar.isSidebarOpen);
 
@@ -21,8 +23,8 @@ const Profile = () => {
         // console.log(isSidebarOpen)
     };
 
-    useEffect(()=>{
-        const getProfileInfo = async () =>{
+    useEffect(() => {
+        const getProfileInfo = async () => {
             try {
                 const response = await axios({
                     url: `${apiUrl}/api/getUser`,
@@ -30,33 +32,67 @@ const Profile = () => {
                     headers: {
                         Authorization: `Bearer ${accessToken}`,
                     },
-                    
+
                 });
-                console.log(response)
+                console.log(response.data.user);
+                setYourData(response.data.user)
+
             } catch (error) {
-                
+
             }
         }
+        
         getProfileInfo()
-    },[])
+    }, [])
 
-  return (
-    <div className='profile-container' >
-       <div className="profile-heading">
-        <h1>YOUR PERSONAL INFORMATION</h1>
-        <span className='menu-bar' onClick={handleToggleSidebar}  >
-            <IoMenuOutline/>
-        </span>
-       </div>
-       <div className="profile-information-container">
+  
+
+    return (
+        <div className='profile-container' >
+            <div className="profile-heading">
+                <h1>YOUR PERSONAL INFORMATION</h1>
+                <span className='menu-bar' onClick={handleToggleSidebar}  >
+                    <IoMenuOutline />
+                </span>
+            </div>
+            <div className="profile-information-container">
+
+                <div className="input-box">
+                    <label >First Name : &nbsp; <p>{yourData.firstName}</p> </label>
+                </div>
+                <div className="input-box">
+                    <label >Last Name : &nbsp; <p>{yourData.lastName}</p> </label>
+                </div>
+                <div className="input-box">
+                    <label >Email : &nbsp; <p>{yourData.email}</p> </label>
+                </div>
+                <div className="input-box">
+                    <label >Phone : &nbsp; <p>{yourData.phone}</p> </label>
+                </div>
+                <div className="input-box">
+                    <label >Department : &nbsp; <p>{yourData.department}</p> </label>
+                </div>
+                <div className="input-box">
+                    <label >Designation : &nbsp; <p>{yourData.designation}</p> </label>
+                </div>
+                
+                <div className="input-box">
+                    <label >Shift : &nbsp; <p>{yourData.shift}</p> </label>
+                </div>
+                <div className="input-box">
+                    <label >Team Lead : &nbsp; <p>{yourData.teamLead}</p> </label>
+                </div>
+                <div className="input-box">
+                    <label >Status : &nbsp; <p>{yourData.status}</p> </label>
+                </div>
+                
+
+            </div>
 
 
-       </div>
 
-
-
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Profile

35e2c7023cfbe2293800d6fd33198f4d3643a5cb
Noor amin <noor.amin@markcoders.com>
2024-06-12 18:20:54 +0500

    fixed reload issue

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,47 +1,63 @@
-import { Outlet,Link } from "react-router-dom"
-import Header from "../components/Header"
-import "../css/layout.css"
-import { CSidebar,CSidebarHeader,CSidebarBrand,CNavLink,CSidebarNav,CNavTitle,CNavItem,CNavGroup,CSidebarToggler, CBadge } from "@coreui/react"
+import { Outlet, Link } from "react-router-dom";
+import Header from "../components/Header";
+import "../css/layout.css";
+import {
+    CSidebar,
+    CSidebarHeader,
+    CNavLink,
+    CSidebarNav,
+    CNavTitle,
+    CNavItem,
+    CSidebarToggler,
+    CNavGroup,
+    cilPuzzle
+} from "@coreui/react";
 import CIcon from '@coreui/icons-react';
-import { cilArrowCircleBottom,cilCalendar,cilUser,cilFlagAlt } from "@coreui/icons";
+import { cilArrowCircleBottom, cilCalendar, cilUser, cilFlagAlt } from "@coreui/icons";
 
 const Layout = () => {
-    
+    return (
+        <>
+            <CSidebar className="border-end" unfoldable={true} colorScheme="dark">
+                <CSidebarHeader className="border-bottom">
+                    <img src="/logo.svg" alt="123" className="img-thumbnail" />
+                </CSidebarHeader>
+                <CSidebarNav>
+                    <CNavTitle>Hresque</CNavTitle>
+                    <CNavItem>
+                        <Link to="/checkin" className="nav-link">
+                            <CIcon customClassName="nav-icon" icon={cilArrowCircleBottom} /> Checkin
+                        </Link>
+                    </CNavItem>
 
-    // return (<>
-    //     <div className="BaseLayout">
-    //         <div className="header-box">
-    //             <Header />
-    //         </div>
-    //         <div className="Outlet">
-    //             <Outlet />
-    //         </div>
-    //     </div>
+                    <CNavGroup toggler={<><CIcon customClassName="nav-icon" icon={cilPuzzle} /> Admin </>}>
+                        <CNavItem><Link to="#"><span className="nav-icon"><span className="nav-icon-bullet"></span></span> Nav dropdown item</Link></CNavItem>
+                        <CNavItem><Link to="#"><span className="nav-icon"><span className="nav-icon-bullet"></span></span> Nav dropdown item</Link></CNavItem>
+                    </CNavGroup>
 
-    // </>)
-
-    return(<>
-        <CSidebar className="border-end" unfoldable={false} colorScheme="dark">
-            <CSidebarHeader className="border-bottom">
-                <img src="/logo.svg" alt="123" className="img-thumbnail"/>
-            </CSidebarHeader>
-            <CSidebarNav>
-              <CNavTitle>Hresque</CNavTitle>
-              <CNavItem href="/checkin"><CIcon customClassName="nav-icon" icon={cilArrowCircleBottom} /> Checkin</CNavItem>
-              {/* <CNavGroup toggler={<><CIcon customClassName="nav-icon" icon={cilPuzzle} /> Admin </>}>
-                <CNavItem href="#"><span className="nav-icon"><span className="nav-icon-bullet"></span></span> Nav dropdown item</CNavItem>
-                <CNavItem href="#"><span className="nav-icon"><span className="nav-icon-bullet"></span></span> Nav dropdown item</CNavItem>
-              </CNavGroup> */}
-              <CNavItem><Link to="/dashboard" className="nav-item"><CIcon customClassName="nav-icon" icon={cilCalendar} /> Attendance</Link></CNavItem>
-              <CNavItem href="/dashboard/profile"><CIcon customClassName="nav-icon" icon={cilUser} /> Profile</CNavItem>
-              <CNavItem href="/dashboard/admin"><CIcon customClassName="nav-icon" icon={cilFlagAlt} /> Admin</CNavItem>
-            </CSidebarNav>
-            <CSidebarHeader className="border-top">
-                <CSidebarToggler />
-            </CSidebarHeader>
-        </CSidebar>
-        <Outlet />
-    </>)
+                    <CNavItem>
+                        <Link to="/dashboard" className="nav-link">
+                            <CIcon customClassName="nav-icon" icon={cilCalendar} /> Attendance
+                        </Link>
+                    </CNavItem>
+                    <CNavItem>
+                        <Link to="/dashboard/profile" className="nav-link">
+                            <CIcon customClassName="nav-icon" icon={cilUser} /> Profile
+                        </Link>
+                    </CNavItem>
+                    <CNavItem>
+                        <Link to="/dashboard/admin" className="nav-link">
+                            <CIcon customClassName="nav-icon" icon={cilFlagAlt} /> Admin
+                        </Link>
+                    </CNavItem>
+                </CSidebarNav>
+                <CSidebarHeader className="border-top">
+                    <CSidebarToggler />
+                </CSidebarHeader>
+            </CSidebar>
+            <Outlet />
+        </>
+    );
 }
 
-export default Layout
+export default Layout;

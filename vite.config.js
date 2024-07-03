751e262f9f2852990271b28c4bc9f5c422bc5df6
Haris <haris.markcoders@gmail.com>
2024-07-03 20:08:31 +0500

    cicd ... maybe

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://hresque.com',
+  base:'https://markcoders.hresque.com',
   plugins: [react()],
   resolve:{
     alias:[{find:"@", replacement:"/src"}]

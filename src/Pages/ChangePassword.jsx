499a84f60094b983cf13ca6c626ab32354622e6d
Noor Amin <noor.amin@markcoders.com>
2024-08-02 20:16:54 +0500

    change password

--- src/Pages/ChangePassword.jsx
+++ src/Pages/ChangePassword.jsx
@@ -1,129 +1,220 @@
-import { Box, Typography } from "@mui/material";
-import { useForm, Controller, useWatch } from "react-hook-form";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
+import { Tabs, Tab, Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { useNavigate, useOutletContext } from "react-router-dom";
 import axiosInstance from "../auth/axiosInstance";
-import { useState } from "react";
-import { LoaderW } from "../components/Loaders";
-import { useParams } from "react-router-dom";
+import { Loader } from "../components/Loaders";
 import CustomTextField from "../components/CustomInput/CustomInput"; // Adjust the import path as needed
 import CustomButton from "../components/CustomButton/CustomButton"; // Adjust the import path as needed
-import '../css/Login.css'; // Assuming you want to use the same CSS as Login
+import SearchIcon from '@mui/icons-material/Search';
+
 const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
-const ChangePassword = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const { token } = useParams();
-    const {
-        control,
-        handleSubmit,
-        formState: { errors },
-        reset,
-    } = useForm();
-    const password = useWatch({ control, name: 'newPassword' });
-    const onSubmit = async (data) => {
-        console.log(data)
-        const { newPassword } = data;
-        try {
-            setIsLoading(true);
-            const response = await axiosInstance.post(`${apiUrl}/api/auth/reset-password`, {
-                password: newPassword,
-                token
-            });
-            setIsLoading(false);
-            toast.success(response.data.message, {
-                position: "top-center",
-            });
-            reset();
-        } catch (error) {
-            console.log(error);
-            setIsLoading(false);
-            const err = error?.response?.data?.message || error.message;
-            toast.error(err, {
-                position: "top-center",
-            });
+
+const EmployeeData = () => {
+    const navigate = useNavigate();
+    const setHeadertext = useOutletContext();
+    const [allEmployee, setAllEmployee] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [filteredEmployees, setFilteredEmployees] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [tabIndex, setTabIndex] = useState(1); // Default to Activated Users
+
+    useEffect(() => {
+        const getAllUser = async () => {
+            try {
+                setHeadertext('User Management');
+                const response = await axiosInstance({
+                    url: `${apiUrl}/api/admin/getAllUsers`,
+                    method: "get",
+                });
+                const dataAllEmployee = response.data;
+                setAllEmployee(dataAllEmployee);
+                setFilteredEmployees(dataAllEmployee.filter(emp => emp.active)); // Default to activated users
+                setLoading(false);
+                console.log(response);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        getAllUser();
+    }, []);
+
+    const handleTabChange = (event, newValue) => {
+        setTabIndex(newValue);
+        if (newValue === 0) {
+            setFilteredEmployees(allEmployee);
+        } else if (newValue === 1) {
+            setFilteredEmployees(allEmployee.filter(emp => emp.active));
+        } else if (newValue === 2) {
+            setFilteredEmployees(allEmployee.filter(emp => !emp.active));
         }
     };
-    const validatePasswordMatch = (value) => value === password || "Passwords do not match";
+
+    const ToggleUserStatus = (rowData) => {
+        const handleToggle = () => {
+            axiosInstance({
+                method: "get",
+                url: `${apiUrl}/api/admin/toggleuseraccount`,
+                params: {
+                    userId: rowData._id
+                }
+            }).then(() => {
+                const updatedEmployees = allEmployee.map(employee =>
+                    employee._id === rowData._id ? { ...employee, active: !employee.active } : employee
+                );
+                setAllEmployee(updatedEmployees);
+                handleTabChange(null, tabIndex); // Refresh the filtered employees
+            });
+        };
+        return (
+            <CustomButton
+                border="1px solid"
+                borderRadius="4px"
+                ButtonText={rowData.active ? "Active" : "Inactive"}
+                fontSize="14px"
+                color={rowData.active ? "green" : "red"}
+                fontWeight="500"
+                fullWidth={false}
+                variant="outlined"
+                padding="5px 10px"
+                onClick={handleToggle}
+                background="#fff"
+                hoverBg="#f5f5f5"
+                hovercolor={rowData.active ? "darkgreen" : "darkred"}
+                type="button"
+            />
+        );
+    };
+
+    const DeleteUser = (rowData) => {
+        const handleDelete = () => {
+            axiosInstance({
+                method: "get",
+                url: `${apiUrl}/api/admin/delete-user`,
+                params: {
+                    id: `${rowData._id}`
+                }
+            }).then(() => {
+                const updatedEmployees = allEmployee.filter(emp => emp._id !== rowData._id);
+                setAllEmployee(updatedEmployees);
+                handleTabChange(null, tabIndex); // Refresh the filtered employees
+            });
+        };
+        return (
+            <CustomButton
+                border="1px solid"
+                borderRadius="4px"
+                ButtonText="Delete"
+                fontSize="14px"
+                color="red"
+                fontWeight="500"
+                fullWidth={false}
+                variant="outlined"
+                padding="5px 10px"
+                onClick={handleDelete}
+                background="#fff"
+                hoverBg="#f5f5f5"
+                hovercolor="darkred"
+                type="button"
+            />
+        );
+    };
+
+    const buttonForViewInformation = (rowData) => {
+        const navigateUserInformation = () => {
+            navigate(`viewInformation/${rowData._id}`);
+        };
+        return (
+            <CustomButton
+                border="1px solid"
+                borderRadius="4px"
+                ButtonText="Details"
+                fontSize="14px"
+                color="blue"
+                fontWeight="500"
+                fullWidth={false}
+                variant="outlined"
+                padding="5px 10px"
+                onClick={navigateUserInformation}
+                background="#fff"
+                hoverBg="#f5f5f5"
+                hovercolor="darkblue"
+                type="button"
+            />
+        );
+    };
+
+    useEffect(() => {
+        const results = allEmployee.filter(employee =>
+            employee.fullName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            employee.email.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredEmployees(results);
+    }, [searchTerm, allEmployee]);
+
     return (
-        <Box className="login-container">
-            <Box>
-                <Typography sx={{
-                    fontWeight: "600",
-                    fontSize: "45px",
-                    color: "#FFFFFF"
-                }}>Change Password</Typography>
-                <Typography sx={{
-                    fontWeight: "400",
-                    fontSize: "16px",
-                    color: "#A0A4A9"
-                }}>Enter your new password below.</Typography>
-            </Box>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Box className="loginFields">
-                    <Typography sx={{ display: "flex", gap: "5px", flexDirection: "column", marginTop: "30px" }}>
-                        <label style={{ color: "white", fontWeight: "400", fontSize: "18px" }}>New Password</label>
-                        <Controller
-                            name="newPassword"
-                            control={control}
-                            defaultValue=""
-                            rules={{ required: "New password is required" }}
-                            render={({ field }) => (
-                                <CustomTextField
-                                    border={true}
-                                    error={errors.newPassword?.message}
-                                    type="password"
-                                    rows={1}
-                                    showPasswordToggle={true}
-                                    {...field}
-                                />
-                            )}
-                        />
-                    </Typography>
-                    <Typography sx={{ display: "flex", gap: "5px", flexDirection: "column", marginTop: "30px" }}>
-                        <label style={{ color: "white", fontWeight: "400", fontSize: "18px" }}>Confirm Password</label>
-                        <Controller
-                            name="confirmPassword"
-                            control={control}
-                            defaultValue=""
-                            rules={{
-                                required: "Confirm password is required",
-                                validate: validatePasswordMatch,
-                            }}
-                            render={({ field }) => (
-                                <CustomTextField
-                                    border={true}
-                                    error={errors.confirmPassword?.message}
-                                    type="password"
-                                    rows={1}
-                                    showPasswordToggle={true}
-                                    {...field}
-                                />
-                            )}
-                        />
-                    </Typography>
-                </Box>
-                <Box sx={{ mt: "60px" }}>
-                    <CustomButton
-                        border="none"
-                        borderRadius="32px"
-                        buttonTextStyle={{ color: "white" }}
-                        buttonStyle={{ color: "white" }}
-                        ButtonText="Change Password"
-                        fontSize="20px"
-                        color="white"
-                        fontWeight="400"
-                        fullWidth={true}
-                        variant="contained"
-                        padding="16px 0"
-                        background="#157AFF"
-                        hoverBg="#157A99F"
-                        hovercolor="white"
-                        type="submit"
-                        loading={isLoading}
+        <Box className='sheet-container-admin'>
+            <Tabs value={tabIndex} onChange={handleTabChange}>
+                <Tab label="All Users" />
+                <Tab label="Activated Users" />
+                <Tab label="Deactivated Users" />
+            </Tabs>
+            <Box className="table-header">
+                <Box className="search-container" sx={{ display: 'flex', alignItems: 'center' }}>
+                    <SearchIcon sx={{ color: 'white', marginRight: 1 }} />
+                    <CustomTextField
+                        placeholder="Search by Name & Email"
+                        type="text"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                     />
                 </Box>
-            </form>
+                <CustomButton
+                    ButtonText="Register User"
+                    fontSize="16px"
+                    color="white"
+                    fontWeight="600"
+                    fullWidth={false}
+                    variant="contained"
+                    padding="10px 20px"
+                    onClick={() => navigate('/dashboard/admin/register')}
+                    background="#3f51b5"
+                    hoverBg="#303f9f"
+                    hovercolor="white"
+                    type="button"
+                />
+            </Box>
+            <Box>
+                {loading ? (
+                    <Box className="loaderContainer"><Loader /></Box>
+                ) : (
+                    <TableContainer>
+                        <Table className="data-table" hover striped>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Full Name</TableCell>
+                                    <TableCell>Email</TableCell>
+                                    <TableCell>View Details</TableCell>
+                                    <TableCell>Toggle Status</TableCell>
+                                    <TableCell>Delete User</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {filteredEmployees.map((employee, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell>{employee.fullName}</TableCell>
+                                        <TableCell>{employee.email}</TableCell>
+                                        <TableCell>{buttonForViewInformation(employee)}</TableCell>
+                                        <TableCell>{ToggleUserStatus(employee)}</TableCell>
+                                        <TableCell>{DeleteUser(employee)}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                )}
+            </Box>
         </Box>
     );
 };
-export default ChangePassword;
+
+export default EmployeeData;
